85ef82524724a8373362a93b13db1e1ce485ed4c
Leandro Cabeza <cabezaleandro@gmail.com>
2023-10-16 19:24:27 -0300

    2da parte funcionando sin guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { authGuard } from './guards/auth.guard';
+import { RepartidorDetalleComponent } from './components/repartidor-detalle/repartidor-detalle.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -12,7 +13,8 @@ const routes: Routes = [
     path: 'altaRepartidor',
     canActivate: [authGuard],
     loadChildren: () => import('./modules/alta-repartidor/alta-repartidor-routing.module').then((m) => m.AltaRepartidorRoutingModule)
-  }
+  },
+  { path: 'repartidorDetalle', component: RepartidorDetalleComponent },
 ];
 
 @NgModule({
