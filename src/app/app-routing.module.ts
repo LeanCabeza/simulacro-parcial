c394eb1be50542d19adb68b007f445c376948f0b
Leandro Cabeza <cabezaleandro@gmail.com>
2023-10-16 17:21:28 -0300

    PRIMERA PARTE PARCIAL TERMINADA

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { AltaRepartidorComponent } from './components/alta-repartidor/alta-repartidor.component';
+import { authGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'altaRepartidor', component: AltaRepartidorComponent },
+  {
+    path: 'altaRepartidor',
+    canActivate: [authGuard],
+    loadChildren: () => import('./modules/alta-repartidor/alta-repartidor-routing.module').then((m) => m.AltaRepartidorRoutingModule)
+  }
 ];
 
 @NgModule({
